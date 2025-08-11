e113bee1f83c4cac15768fa5586d0c2562918990
Alessio Magliarella <49778555+magliaral@users.noreply.github.com>
2025-08-11 15:48:10 +0200

    chore: history cleanup

--- custom_components/bacnet_hub/config_flow.py
+++ custom_components/bacnet_hub/config_flow.py
@@ -10,6 +10,9 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input: dict[str, Any] | None = None):
+        await self.async_set_unique_id("bacnet_hub_singleton")
+        self._abort_if_unique_id_configured()
+
         if user_input is not None:
             return self.async_create_entry(title="BACnet Hub", data=user_input)
 
