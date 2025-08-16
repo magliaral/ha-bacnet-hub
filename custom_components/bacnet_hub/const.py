d3cc9cf35bc20415636c345b9480e789c72c1489
Alessio Magliarella <49778555+magliaral@users.noreply.github.com>
2025-08-16 09:25:35 +0200

    chore: history cleanup

--- custom_components/bacnet_hub/const.py
+++ custom_components/bacnet_hub/const.py
@@ -1,2 +1,10 @@
 DOMAIN = "bacnet_hub"
 DEFAULT_NAME = "BACnet Hub"
+
+CONF_ADDRESS = "address"
+CONF_PORT = "port"
+CONF_INSTANCE = "instance"
+CONF_DEVICE_NAME = "device_name"
+CONF_BBMD_IP = "bbmd_ip"
+CONF_BBMD_TTL = "bbmd_ttl"
+CONF_OBJECTS_YAML = "objects_yaml"
