bcddff1b2fe05356f55bb10962b6e07a2418c1b2
Alessio Magliarella <49778555+magliaral@users.noreply.github.com>
2025-08-23 11:22:09 +0200

    Update sensor.py

--- custom_components/bacnet_hub/sensor.py
+++ custom_components/bacnet_hub/sensor.py
@@ -54,8 +54,8 @@ class BacnetPublishedSensor(SensorEntity):
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, entry_id)},
             name="BACnet Hub (Local Device)",
-            manufacturer="Home Assistant",
-            model="BACpypes 3",
+            manufacturer="magliaral",
+            model="BACpypes 3 - JoelBender",
         )
 
     async def async_added_to_hass(self) -> None:
